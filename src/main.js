361bd585dd75e1de48a988e4815d4f4b1faaea6e
Abhishek Gupta <hi@itsabhishek.me>
2019-08-15 00:41:21 +0530

    fix carousel

--- src/main.js
+++ src/main.js
@@ -1,3 +1,6 @@
 import CBNavbar from './cb-navbar/index.svelte'
 import UpcomingContests from './upcoming-contests/index.svelte'
+
 import "@coding-blocks/motley/dist/hb/app.min.css"
+
+import "macro-carousel"
